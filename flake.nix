641f2e3e0a3231d22b595aa9a09d926a7b62f99e
kleha <kleha@klefpj.org>
2026-07-20 14:30:28 +0900

    Fix Username

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
          {
            home-manager.useGlobalPkgs = true;
            home-manager.useUserPackages = true;
-           home-manager.users.kleha = import ./home-wsl.nix;
+           home-manager.users.nixos = import ./home-wsl.nix;
            home-manager.backupFileExtension = "bkup";
          }
        ];
